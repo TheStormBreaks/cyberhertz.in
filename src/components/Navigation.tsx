d4c53cc02bfab6566e24d83ecf032ad417df1a5e
Somya <m2somya@gmail.com>
2025-04-28 17:24:54 +0530

    live

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -34,7 +34,7 @@ export function Navigation() {
             href="/mb"
             className={`navigation-item relative group ${pathname === '/about' ? 'text-white' : 'text-white/70'} hover:text-white transition-colors`}
           >
-            <span className="header-link">Mrigank</span>
+            <span className="header-link">Live Projects</span>
             {pathname === '/mb' && <span className="absolute -bottom-1 left-0 w-full h-[2px] bg-white"></span>}
             <span className="absolute -bottom-1 left-0 w-0 h-[2px] bg-white group-hover:w-full transition-all duration-300"></span>
           </Link>
